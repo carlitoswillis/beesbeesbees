de6a5ba2b49821e89c07a8c2e109d1d1dcf96d2c
carlitoswillis <carlitoswillis@berkeley.edu>
2020-03-30 12:05:40 -0700

    Implemented a lot of Bees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,27 @@
 var RetiredForagerBee = function() {
-};
+  ForagerBee.call(this);
+  this.age = 40;
+  this.canFly = false;
+  this.color = 'grey';
+  this.job = 'gamble';
 
+
+};
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function () {
+  return "I am too old, let me play cards instead"
+};
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure);
+}
+
+// an age property that is set to 40
+// a job property that is set to gamble
+// a canFly property that is set to false
+// a color property that is set to grey
+// a forage method that returns "I am too old, let me play cards instead"
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a treasureChest property inherited from ForagerBee that is set to an empty array []
+// an always winning gamble method that allows the bee to add a treasure to the treasureChest
